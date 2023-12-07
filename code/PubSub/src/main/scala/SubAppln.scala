8562cd34d2db524df741cb9275c3017daf3c5f49
Youngjae Moon <pingumoon18@gmail.com>
2023-12-07 12:21:31 -0600

    update

--- code/PubSub/src/main/scala/SubAppln.scala
+++ code/PubSub/src/main/scala/SubAppln.scala
@@ -1,21 +1,56 @@
-object SubAppln {
-  def subscribe(topic: String, callback: (Any) => Unit): Unit = {
-    // Subscribe to a topic and define a callback
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import scala.util.control.NonFatal
+
+class SubAppln extends SubUpcallHandler {
+  private val logger: Logger = LoggerFactory.getLogger(getClass)
+  private val mw_obj = SubMW
+  mw_obj.set_upcall_handle(this)
+
+  // State variables
+  private var name: String = _
+  private var subscribedExchanges: Set[String] = Set()
+
+  def init(name: String, exchanges: Set[String]): Unit = {
+    this.name = name
+    this.subscribedExchanges = exchanges
   }
-  
-  def unsubscribe(topic: String): Unit = {
-    // Unsubscribe from a specific topic
+
+  def subscribe(exchangeName: String): Unit = {
+    mw_obj.setup_subscriber_queue(exchangeName)
+    subscribedExchanges += exchangeName
   }
-  
+
+  def unsubscribe(exchangeName: String): Unit = {
+    mw_obj.unsubscribe(exchangeName)
+    subscribedExchanges -= exchangeName
+  }
+
+  def change_subscription(newExchangeName: String): Unit = {
+    subscribedExchanges.foreach(unsubscribe)
+    subscribe(newExchangeName)
+  }
+
   def disconnect(): Unit = {
-    // Unsubscribe from the current subscription
+    mw_obj.disconnect()
   }
-  
-  def change_subscription(topics: Set[String]): Unit = {
-    // Change the set of subscribed topics
+
+  override def onMessageReceived(message: String): Unit = {
+    logger.info(s"Message received: $message")
+    // Implement additional logic for handling received messages if needed
   }
-  
-  def handle_listen_errors(error: Throwable): Unit = {
-    // Handle errors during message listening
+
+  override def onErrorOccurred(error: Throwable): Unit = {
+    logger.error("Error occurred in SubMW", error)
+  }
+
+  def dump(): Unit = {
+    try {
+      logger.info("SubAppln::dump")
+      logger.info(s"-Name: $name")
+      logger.info(s"-Subscribed Exchanges: ${subscribedExchanges.mkString(", ")}")
+    } catch {
+      case NonFatal(e) => logger.error("Error during dump", e)
+    }
   }
 }
--- code/PubSub/src/main/scala/SubMW.scala
+++ code/PubSub/src/main/scala/SubMW.scala
@@ -1,19 +1,28 @@
 import com.rabbitmq.client.{AMQP, Channel, Connection, ConnectionFactory, DefaultConsumer, Envelope, MessageProperties}
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+trait SubUpcallHandler {
+  def onMessageReceived(message: String): Unit
+  def onErrorOccurred(error: Throwable): Unit
+}
 
 object SubMW {
   private var connection: Connection = _
   private var channel: Channel = _
   private var queueName: String = _
+  private var upcallHandler: Option[SubUpcallHandler] = None
+
+  private val objectMapper = new ObjectMapper().registerModule(DefaultScalaModule)
 
   def connect_to_server(host: String, port: Int): Unit = {
     val factory = new ConnectionFactory()
     factory.setHost(host)
     factory.setPort(port)
-    connection = factory.newConnection() // Corrected method call
+    connection = factory.newConnection()
     channel = connection.createChannel()
   }
 
-  def setup_subscriber_queue(exchangeName: String, callback: String => Unit): Unit = {
+  def setup_subscriber_queue(exchangeName: String): Unit = {
     queueName = channel.queueDeclare().getQueue
     channel.queueBind(queueName, exchangeName, "")
 
@@ -21,14 +30,37 @@ object SubMW {
       override def handleDelivery(consumerTag: String, envelope: Envelope, properties: AMQP.BasicProperties, body: Array[Byte]): Unit = {
         try {
           val message = new String(body, "UTF-8")
-          callback(message)
+          val deserializedMessage : String = objectMapper.readValue(message, classOf[String]) 
+          upcallHandler.foreach(_.onMessageReceived(deserializedMessage))
         } catch {
           case e: Exception =>
-            println(s"Error while processing message: ${e.getMessage}")
+            upcallHandler.foreach(_.onErrorOccurred(e))
         }
       }
     }
+  }
+    
+  def set_upcall_handle(handler: SubUpcallHandler): Unit = {
+    upcallHandler = Some(handler)
+  }
 
-    channel.basicConsume(queueName, true, consumer)
+  def unsubscribe(exchangeName: String): Unit = {
+    if (channel != null && queueName != null) {
+      // Unbind the queue from the specified exchange
+      channel.queueUnbind(queueName, exchangeName, "")
+    }
+  }
+
+  def change_subscription(newExchangeName: String): Unit = {
+    unsubscribe("")
+    setup_subscriber_queue(newExchangeName)
+  }
+
+  def disconnect(): Unit = {
+    if (channel != null && queueName != null) {
+      channel.queueDelete(queueName)
+      channel.close()
+    }
+    if (connection != null) connection.close()
   }
 }
